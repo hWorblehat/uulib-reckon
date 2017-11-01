8b17a339497f6ccf060c16b19001487ea1152d92
hWorblehat <hWorblehat@users.noreply.github.com>
2017-11-01 16:18:39 +0000

    Move plugin class into 'gradle' package

--- gradle-plugin/src/main/groovy/org/uulib/reckon/gradle/ReckonGradlePlugin.groovy
+++ gradle-plugin/src/main/groovy/org/uulib/reckon/gradle/ReckonGradlePlugin.groovy
@@ -1,15 +1,14 @@
-package org.uulib.reckon;
+package org.uulib.reckon.gradle;
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.uulib.reckon.dsl.Reckon
-import org.uulib.reckon.gradle.ReckonExtension
 
 public class ReckonGradlePlugin implements Plugin<Project> {
 
 	@Override
 	public void apply(Project project) {
-		def ext = project.extensions.create('reckon', ReckonExtension)
+		def ext = project.extensions.create('reckon', ReckonExtension, project)
 		project.version = ext.reckon({})
 	}
 
